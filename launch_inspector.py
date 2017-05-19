aa411eb55104812bb9fc1114da4b52210da5dd7d
Ivan Vishniakou <ivan.vishniakou@smail.inf.h-brs.de>
2017-05-19 12:48:22 +0200

    Work on pygraphviz in progress

--- launch_inspector.py
+++ launch_inspector.py
@@ -8,6 +8,26 @@ import xdot
 import pygraphviz as gv
 
 
+class RINode(object):
+    """
+    This class wraps pygraphviz nodes and takes care of
+    their layout
+    """
+    #Unique id for the node
+    uid_counter = 0
+    
+    def __init__(self):
+        self._uid = RINode.uid_counter
+        RINode.uid_counter += 1
+    
+    def add_to_graph(self, gv_graph):
+        gv_graph.add_node(self._uid)
+        
+    def get_gv_attr(self):
+        params = {}
+        
+
+        
 class RoslaunchInspector(wx.Frame):
     """
     This class provides a GUI application for viewing roslaunch files.
@@ -16,17 +36,23 @@ class RoslaunchInspector(wx.Frame):
         wx.Frame.__init__(self, None, -1, "Roslaunch inspector")
         self.graph_view = xdot.wxxdot.WxDotWindow(self, -1)
         self.graph_view.set_filter('neato')
+        
         if not dot_filename is None:
             graph = gv.AGraph(filename=dot_filename)
+            graph.node = {'shape':'record'}
         else:
             print 'no_file'
-            graph = gv.AGraph()
-            graph.add_node('a', pos='0,0')
+            graph = gv.AGraph(rankdir='LR')
+            graph.add_node('a', shape='record', label='Launchfile|{params|{a|s}|b}|3', pos='0,0!')
+
+            #graph.add_subgraph(nbunch=nlist, name='asd', label='s')
+            #n = RINode()
+            #n.add_to_graph(graph)
             
-            graph.add_node('b', pos='10,10', label='test')
-            
-            self.graph_view.set_dotcode(graph.to_string())
-            print graph
+        self.graph_view.set_dotcode(graph.to_string())
+        print graph
+        
+                
 
 
 def main(argv):
